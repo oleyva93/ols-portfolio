d2c1b3ac12b013e2ad8ad5f981f1c373bab8a7a2
oleyva93 <oleyva930424@gmail.com>
2023-10-08 01:15:04 -0600

    Fix edge config

--- src/app/api/me/route.js
+++ src/app/api/me/route.js
@@ -1,8 +1,6 @@
 import { ImageResponse } from "next/server";
 
-export const config = {
-  runtime: "edge",
-};
+export const runtime = "edge";
 
 export async function GET() {
   return new ImageResponse(
